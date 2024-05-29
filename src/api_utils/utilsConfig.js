8b722a500e31d4e36cc830a00ecad7451e10a50e
thesandfox <sandfox9411@gmail.com>
2024-05-29 14:08:45 +0900

    240529

--- src/api_utils/utilsConfig.js
+++ src/api_utils/utilsConfig.js
@@ -1,9 +1,11 @@
-console.log(import.meta.env.VITE_DEBUG+"!!!");
+console.log(String(import.meta.env.VITE_DEBUG)+"!!");
 const BASE_URL = 
-	import.meta.env.VITE_DEBUG==="true"
+	import.meta.env.VITE_DEBUG
 	//백엔드 어플리케이션 URL
 	?'http://localhost:3001/api'//로컬
 	:'https://port-0-bandifesta-rest-ss7z32llwjy3ukc.sel5.cloudtype.app/api'//배포된거
+console.log(BASE_URL);
+console.log('낄낄');
 
 export {
 	BASE_URL
