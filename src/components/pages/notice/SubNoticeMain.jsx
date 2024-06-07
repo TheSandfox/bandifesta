8cf24059676e0d1cb22de76d6bfa6c91c56bfe8e
pcm5433 <pcm5433@naver.com>
2024-06-07 09:21:15 +0900

    123

--- src/components/pages/notice/SubNoticeMain.jsx
+++ src/components/pages/notice/SubNoticeMain.jsx
@@ -1,4 +1,99 @@
-export default function SubNoticeMain({}) {
-	return <>
-	</>
+import './SubNoticeMain.css'
+
+function SubNoticeMain({}) {
+	return(
+        <section className='noticeWrap'>
+            <div className="noticeTab">
+                <div className="noticeTabTit">알려드립니다</div>
+                <div className="noticeTabSub">공지사항</div>
+                <div className="noticeTabSub">자주하는 질문</div>
+            </div>
+            <div className='noticeMain'>
+                <div className='searchWrap'></div>
+                <p>총 <span>10</span>개의 게시글이 있습니다.</p>
+                <div>
+                    <ul className='noticeListT'>
+                        <li className='notiNumber'>번호</li>
+                        <li className='notiTitle'>제목</li>
+                        <li className='notiWriter'>작성자</li>
+                        <li className='notiDate'>작성일</li>
+                        <li className='notiView'>조회수</li>
+                    </ul>
+                    <ul className='noticeListM'>
+                        <li className='notiNumber'>01</li>
+                        <li className='notiTitle'>2024 부여 문화유산 야행 실시간 문의사항 오픈채팅</li>
+                        <li className='notiWriter'>관리자</li>
+                        <li className='notiDate'>2024-04-12</li>
+                        <li className='notiView'>369</li>
+                    </ul>
+                    <ul className='noticeListM'>
+                        <li className='notiNumber'>01</li>
+                        <li className='notiTitle'>2024 부여 문화유산 야행 실시간 문의사항 오픈채팅</li>
+                        <li className='notiWriter'>관리자</li>
+                        <li className='notiDate'>2024-04-12</li>
+                        <li className='notiView'>369</li>
+                    </ul>
+                    <ul className='noticeListM'>
+                        <li className='notiNumber'>01</li>
+                        <li className='notiTitle'>2024 부여 문화유산 야행 실시간 문의사항 오픈채팅</li>
+                        <li className='notiWriter'>관리자</li>
+                        <li className='notiDate'>2024-04-12</li>
+                        <li className='notiView'>369</li>
+                    </ul>
+                    <ul className='noticeListM'>
+                        <li className='notiNumber'>01</li>
+                        <li className='notiTitle'>2024 부여 문화유산 야행 실시간 문의사항 오픈채팅</li>
+                        <li className='notiWriter'>관리자</li>
+                        <li className='notiDate'>2024-04-12</li>
+                        <li className='notiView'>369</li>
+                    </ul>
+                    <ul className='noticeListM'>
+                        <li className='notiNumber'>01</li>
+                        <li className='notiTitle'>2024 부여 문화유산 야행 실시간 문의사항 오픈채팅</li>
+                        <li className='notiWriter'>관리자</li>
+                        <li className='notiDate'>2024-04-12</li>
+                        <li className='notiView'>369</li>
+                    </ul>
+                    <ul className='noticeListM'>
+                        <li className='notiNumber'>01</li>
+                        <li className='notiTitle'>2024 부여 문화유산 야행 실시간 문의사항 오픈채팅</li>
+                        <li className='notiWriter'>관리자</li>
+                        <li className='notiDate'>2024-04-12</li>
+                        <li className='notiView'>369</li>
+                    </ul>
+                    <ul className='noticeListM'>
+                        <li className='notiNumber'>01</li>
+                        <li className='notiTitle'>2024 부여 문화유산 야행 실시간 문의사항 오픈채팅</li>
+                        <li className='notiWriter'>관리자</li>
+                        <li className='notiDate'>2024-04-12</li>
+                        <li className='notiView'>369</li>
+                    </ul>
+                    <ul className='noticeListM'>
+                        <li className='notiNumber'>01</li>
+                        <li className='notiTitle'>2024 부여 문화유산 야행 실시간 문의사항 오픈채팅</li>
+                        <li className='notiWriter'>관리자</li>
+                        <li className='notiDate'>2024-04-12</li>
+                        <li className='notiView'>369</li>
+                    </ul>
+                    <ul className='noticeListM'>
+                        <li className='notiNumber'>01</li>
+                        <li className='notiTitle'>2024 부여 문화유산 야행 실시간 문의사항 오픈채팅</li>
+                        <li className='notiWriter'>관리자</li>
+                        <li className='notiDate'>2024-04-12</li>
+                        <li className='notiView'>369</li>
+                    </ul>
+                    <ul className='noticeListM'>
+                        <li className='notiNumber'>01</li>
+                        <li className='notiTitle'>2024 부여 문화유산 야행 실시간 문의사항 오픈채팅</li>
+                        <li className='notiWriter'>관리자</li>
+                        <li className='notiDate'>2024-04-12</li>
+                        <li className='notiView'>369</li>
+                    </ul>
+                </div>
+                <button>글쓰기</button>
+            </div>
+        </section>
+    )
 }
+
+export default SubNoticeMain;
