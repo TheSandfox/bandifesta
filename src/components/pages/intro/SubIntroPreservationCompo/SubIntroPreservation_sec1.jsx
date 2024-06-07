097e9bc668f83d936661a3f27ea5c92232a813d9
Sauser <gubonil13@naver.com>
2024-06-07 11:02:25 +0900

    7일 2차

--- src/components/pages/intro/SubIntroPreservationCompo/SubIntroPreservation_sec1.jsx
+++ src/components/pages/intro/SubIntroPreservationCompo/SubIntroPreservation_sec1.jsx
@@ -1,24 +1,59 @@
 import "./SubIntroPreservation_sec1.css";
-export default function SubIntroPreservation_sec1({ }) {
+export default function SubIntroPreservation_sec1({}) {
   return (
     <>
       <div className="SubIntroPreservation_sec1_wrap">
         <div className="SubIntroPreservation_sec1_top">
           <div>
-            <h3>경복궁 별빛야행 하반기 예매권<br />
+            <h3>
+              경복궁 별빛야행 하반기 예매권
+              <br />
               추첨 방식을 알려드립니다.
             </h3>
           </div>
         </div>
-        <div className="SubIntroPreservation_sec1_second">
-          <div className="">
-            <div><img src="/bandifesta/public/assets/highlight1.png" alt="별모양" /></div>
+        <div className="SubIntroPreservation_sec1_first">
+          <div className="flex SubIntroPreservation_sec1_title">
+            <div>
+              <img src="/bandifesta/assets/highlight1.png" alt="별모양" />
+            </div>
             <h5>예매권 추첨이란?</h5>
           </div>
-          <div>
-            <p></p>
+          <div className="SubIntroPreservation_sec1_first_cont">
+            <p>
+              경복궁 별빛야행 참가 희망자 중 추첨을 통해 공정한 체험 기회를
+              드리고자 준비한 예매 방법으로
+              <br />
+              <b>티켓링크 (www.ticketlink.co.kr)에서 ID당 한 번의 응모 기회</b>
+              를 드리며,추첨을 통해
+              <br />
+              <b>예매권에 선정된 관람객은 희망 날짜와 회차를 선착순으로 예매</b>
+              를 할 수 있습니다.
+            </p>
           </div>
-          <p></p>
+          <p>* 아래 안내사항을 꼭 확인하고 예매해주세요.</p>
+        </div>
+        <div className="flex SubIntroPreservation_sec1_second">
+          <div className="flex SubIntroPreservation_sec1_title">
+            <div>
+              <img src="/bandifesta/assets/highlight1.png" alt="별모양" />
+            </div>
+            <h5>예매권 추첨 및 예매 일정</h5>
+          </div>
+          <div className="flex SubIntroPreservation_sec1_second_cont">
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
         </div>
       </div>
     </>
--- src/components/pages/intro/introMainCompo/SubIntroMainSec1.jsx
+++ src/components/pages/intro/introMainCompo/SubIntroMainSec1.jsx
@@ -6,7 +6,7 @@ export default function SubIntroMainSec1({}) {
         <div>
           <h3>밤 깊은 경복궁에 여러분을 초대합니다.</h3>
           <div className="SubIntroMainSec1_img1">
-            <img src="/bandifesta/public/assets/night_star_logo.png"></img>
+            <img src="/bandifesta/assets/night_star_logo.png"></img>
           </div>
           <p>
             경복궁의 가장 깊은 곳<br />
