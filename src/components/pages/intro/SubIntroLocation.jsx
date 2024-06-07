a87a18b9eab5cefbae8bd58f9359f33c5b8f0f79
Sauser <gubonil13@naver.com>
2024-06-07 16:43:36 +0900

    zjalt

--- src/components/pages/intro/SubIntroLocation.jsx
+++ src/components/pages/intro/SubIntroLocation.jsx
@@ -1,11 +1,14 @@
 import { useEffect } from "react";
 import IntroTitle from "./intro_components/IntroTitle"
+import MyComponent from "../../generic/googlemap/MyComponent"
+import SubIntroLocation_sec1 from './SubIntroLocation/SubIntroLocation_sec1'
 export default function SubIntroLocation({handleTabState,index}) {
 	useEffect(()=>{
 		handleTabState.set(index);
 	},[])
 	return <>
-			<IntroTitle introTitle={"프로그램 소개"}/>
-			
+			<IntroTitle introTitle={"오시는 길"}/>
+			<MyComponent />
+			<SubIntroLocation_sec1 />
 	</>
 }
