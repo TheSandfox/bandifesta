4bae7f3300cd6f9e58f48bd4a37a8d051054b78a
gihoon <gihoon@gihoon.info>
2024-06-07 09:30:21 +0900

    주말작업 ㅠㅠ

--- src/components/pages/intro/SubIntroPreservation.jsx
+++ src/components/pages/intro/SubIntroPreservation.jsx
@@ -1,3 +1,18 @@
+import IntroTitle from "./intro_components/IntroTitle"
+import SubIntroPreservation_sec1 from "./SubIntroPreservationCompo/SubIntroPreservation_sec1"
+// import SubIntroPreservation_sec2 from "./SubIntroPreservationCompo/SubIntroPreservation_sec2"
+// import SubIntroPreservation_sec3 from "./SubIntroPreservationCompo/SubIntroPreservation_sec3"
+// import SubIntroPreservation_sec4 from "./SubIntroPreservationCompo/SubIntroPreservation_sec4"
+// import SubIntroPreservation_sec5 from "./SubIntroPreservationCompo/SubIntroPreservation_sec5"
+// import SubIntroPreservation_sec6 from "./SubIntroPreservationCompo/SubIntroPreservation_sec6"
 export default function SubIntroPreservation({}) {
-	return <></>
+	return <>
+	<IntroTitle introTitle={"예매안내"}/>
+	<SubIntroPreservation_sec1/>
+	{/* <SubIntroPreservation_sec2/>
+	<SubIntroPreservation_sec3/>
+	<SubIntroPreservation_sec4/>
+	<SubIntroPreservation_sec5/>
+	<SubIntroPreservation_sec6/> */}
+	</>
 }
---
+++ src/components/pages/intro/SubIntroPreservationCompo/SubIntroPreservation_sec1.jsx
@@ -0,0 +1,26 @@
+import "./SubIntroPreservation_sec1.css";
+export default function SubIntroPreservation_sec1({ }) {
+  return (
+    <>
+      <div className="SubIntroPreservation_sec1_wrap">
+        <div className="SubIntroPreservation_sec1_top">
+          <div>
+            <h3>경복궁 별빛야행 하반기 예매권<br />
+              추첨 방식을 알려드립니다.
+            </h3>
+          </div>
+        </div>
+        <div className="SubIntroPreservation_sec1_second">
+          <div>
+            <div><img src="/bandifesta/public/assets/highlight 1" alt="" /></div>
+            <h5>예매권 추첨이란?</h5>
+          </div>
+          <div>
+            <p></p>
+          </div>
+          <p></p>
+        </div>
+      </div>
+    </>
+  );
+}
