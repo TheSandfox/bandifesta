f7e755b55890a6df3e1b4614883bd53acb70af53
thesandfox <sandfox9411@gmail.com>
2024-06-07 09:22:48 +0900

    240607

---
+++ src/components/generic/SsangTab.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom'
+import './ssangtab.css'
+
+function SsangTab({children,to,active}) {
+	return <Link className={`ssangTab fontSubTitle${active?' active':''}`} to={to}>
+		{children}
+	</Link>
+}
+
+function SsangTabContainer({children}) {
+	return <div className='ssangTabContainer'>
+		{children}
+	</div>
+}
+
+export {
+	SsangTabContainer,
+	SsangTab
+}
--- src/components/pages/festival/PageFestival.jsx
+++ src/components/pages/festival/PageFestival.jsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom"
 import SubFestivalGallery from "./SubFestivalGallery";
 import { useState } from "react";
 import SubFestivalSchedule from "./SubFestivalSchedule";
-import { LeftTab, LeftTabContainer, LeftTabTitle } from "../../generic/LeftTab";
+import { SsangTab, SsangTabContainer } from "../../generic/SsangTab";
 
 export default function PageFestival({}) {
 	const { tabName } = useParams();
@@ -24,11 +24,10 @@ export default function PageFestival({}) {
 	}
 	return <div className="mainContent">
 		<div className="tabContentDivision">
-			<LeftTabContainer>
-				<LeftTabTitle>끼루룩끼힝</LeftTabTitle>
-				<LeftTab to={'/festival/gallery'} active={tabState===0}>갤러리뷰</LeftTab>
-				<LeftTab to={'/festival/schedule'} active={tabState===1}>스케쥴뷰</LeftTab>
-			</LeftTabContainer>
+			<SsangTabContainer>
+				<SsangTab to={'/festival/gallery'} active={tabState===0}>갤러리뷰</SsangTab>
+				<SsangTab to={'/festival/schedule'} active={tabState===1}>스케쥴뷰</SsangTab>
+			</SsangTabContainer>
 			<div>
 				{jsx}
 			</div>
