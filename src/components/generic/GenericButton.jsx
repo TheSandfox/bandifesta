db8b3f49e7b1801a4e47dade1de45ead48bd0937
thesandfox <sandfox9411@gmail.com>
2024-06-10 12:54:53 +0900

    240610

--- src/components/generic/GenericButton.jsx
+++ src/components/generic/GenericButton.jsx
@@ -1,7 +1,12 @@
-import './gnericbutton.css'
+import { useNavigate } from 'react-router'
+import './genericbutton.css'
 
-export default function GenericButton({children, onClick}) {
-	return <div className="genericButton" onClick={onClick||(()=>{})}>
+export default function GenericButton({children, to, onClick}) {
+	const navigate = useNavigate();
+	const navigateCallback = ()=>{
+		if (to) {navigate(to);}
+	}
+	return <div className="genericButton" onClick={onClick?onClick:navigateCallback}>
 		<div className="fontMain">
 			{children}
 		</div>
