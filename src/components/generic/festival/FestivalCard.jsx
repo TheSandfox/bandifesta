5dc2a22593703fdabe97406af0b4da7a45d480dd
Sauser <gubonil13@naver.com>
2024-06-11 14:24:46 +0900

    06110224

--- src/components/generic/festival/FestivalCard.jsx
+++ src/components/generic/festival/FestivalCard.jsx
@@ -1,115 +1,122 @@
-import { Link } from 'react-router-dom';
-import './festival.css';
-import { useEffect, useRef, useState } from 'react';
-import GenericTag from '../GenericTag';
+import { Link } from "react-router-dom";
+import "./festival.css";
+import { useEffect, useRef, useState } from "react";
+import GenericTag from "../GenericTag";
 
-function FestivalLikeButton({festivalId,userId}) {
-	const [pressed,setPressed] = useState(false);
-	const handlePressed = {
-		toggle:()=>{
-			setPressed(!pressed);
-		}
-	}
-	return <div className='festivalLikeButton' onClick={handlePressed.toggle}>
-		<img className={'heart'} src={`/bandifesta/assets/${pressed?'heartFill':'heart'}.png`} alt={'축제 좋아요 버튼'}/>
-	</div>
+function FestivalLikeButton({ festivalId, userId }) {
+  const [pressed, setPressed] = useState(false);
+  const handlePressed = {
+    toggle: () => {
+      setPressed(!pressed);
+    },
+  };
+  return (
+    <div className="festivalLikeButton" onClick={handlePressed.toggle}>
+      <img
+        className={"heart"}
+        src={`/bandifesta/assets/${pressed ? "heartFill" : "heart"}.png`}
+        alt={"축제 좋아요 버튼"}
+      />
+    </div>
+  );
 }
 
-function FestivalCard({festival}) {
-	const imgElement = useRef(null);
-	const [tagVariation,setTagVariation] = useState({
-		value:0,
-		string:''
-	});
-	//진,예,마 판별
-	useEffect(()=>{
-		let startDate = new Date(festival.start_date);
-		let today = new Date();
-		let endDate = new Date(festival.end_date);
-		if(startDate>today) {
-			setTagVariation({
-				value:1,
-				string:'예정'
-			})
-		} else if(endDate<today) {
-			setTagVariation({
-				value:2,
-				string:'마감'
-			})
-		} else {
-			setTagVariation({
-				value:0,
-				string:'진행중'
-			})
-		}
-	},[])
-	return <div className='festivalCard'>
-		<div className='festivalCardTop'>
-			<img src={festival.image1}
-				alt={festival.title} 
-				className='festivalCardImage'
-				ref={imgElement}/>
-			<FestivalLikeButton/>
-		</div>
-		{/* 진,예,마 태그 */}
-		<div className=''>
-			<GenericTag variation={tagVariation.value}>
-				{tagVariation.string}
-			</GenericTag>
-		</div>
-		<div className='fontSubTitle'>
-			{festival.title}
-		</div>
-	</div>
+function FestivalCard({ festival }) {
+  const imgElement = useRef(null);
+  const [tagVariation, setTagVariation] = useState({
+    value: 0,
+    string: "",
+  });
+  //진,예,마 판별
+  useEffect(() => {
+    let startDate = new Date(festival.start_date);
+    let today = new Date();
+    let endDate = new Date(festival.end_date);
+    if (startDate > today) {
+      setTagVariation({
+        value: 1,
+        string: "예정",
+      });
+    } else if (endDate < today) {
+      setTagVariation({
+        value: 2,
+        string: "마감",
+      });
+    } else {
+      setTagVariation({
+        value: 0,
+        string: "진행중",
+      });
+    }
+  }, []);
+  return (
+    <div className="festivalCard">
+      <div className="festivalCardTop">
+        <img
+          src={festival.image1}
+          alt={festival.title}
+          className="festivalCardImage"
+          ref={imgElement}
+        />
+        <FestivalLikeButton />
+      </div>
+      {/* 진,예,마 태그 */}
+      <div className="">
+        <GenericTag variation={tagVariation.value}>
+          {tagVariation.string}
+        </GenericTag>
+      </div>
+      <div className="fontSubTitle">{festival.title}</div>
+    </div>
+  );
 }
 
-function FestivalCardList({festivals}) {
-	return <div className="festivalCardList">
-		{festivals.map((festival)=>{
-			return <FestivalCard key={festival.festival_id} festival={festival}/>
-		})}
-	</div>
-	// const config = useContext(configContext)
-	// const [festivals,setFestivals] = useState({
-	// 	loaded:false,
-	// 	items:[]
-	// });
-	// //언어 변경 시
-	// useEffect(()=>{
-	// 	setFestivals({
-	// 		loaded:false,
-	// 		items:[]
-	// 	})
-	// 	getOngoingFestivals({
-	// 		itemsPerPage:10,
-	// 		pageNum:1,
-	// 		language:config.language
-	// 	},(response)=>{
-	// 		console.log(response);
-	// 		setFestivals({
-	// 			loaded:true,
-	// 			items:response.data
-	// 		})
-	// 	})
-	// },[config.language]);
-	// return (
-	// 	<>
-	// 		{
-	// 			!festivals.loaded
-	// 			?'로딩중'
-	// 			:(
-	// 				festivals.items.length>0
-	// 				?festivals.items.map((item)=>{
-	// 					return <FestivalCard key={item.festival_id} festival={item}/>
-	// 				})
-	// 				:'...'
-	// 			)
-	// 		}
-	// 	</>
-	// )
+function FestivalCardList({ festivals }) {
+  return (
+    <div className="festivalCardList">
+      {festivals.map((festival) => {
+        return <FestivalCard key={festival.festival_id} festival={festival} />;
+      })}
+    </div>
+  );
+  // const config = useContext(configContext)
+  // const [festivals,setFestivals] = useState({
+  // 	loaded:false,
+  // 	items:[]
+  // });
+  // //언어 변경 시
+  // useEffect(()=>{
+  // 	setFestivals({
+  // 		loaded:false,
+  // 		items:[]
+  // 	})
+  // 	getOngoingFestivals({
+  // 		itemsPerPage:10,
+  // 		pageNum:1,
+  // 		language:config.language
+  // 	},(response)=>{
+  // 		console.log(response);
+  // 		setFestivals({
+  // 			loaded:true,
+  // 			items:response.data
+  // 		})
+  // 	})
+  // },[config.language]);
+  // return (
+  // 	<>
+  // 		{
+  // 			!festivals.loaded
+  // 			?'로딩중'
+  // 			:(
+  // 				festivals.items.length>0
+  // 				?festivals.items.map((item)=>{
+  // 					return <FestivalCard key={item.festival_id} festival={item}/>
+  // 				})
+  // 				:'...'
+  // 			)
+  // 		}
+  // 	</>
+  // )
 }
 
-export {
-	FestivalCardList,
-	FestivalCard,
-}
+export { FestivalCardList, FestivalCard };
--- src/components/pages/main/main_components/sec4/sec4.jsx
+++ src/components/pages/main/main_components/sec4/sec4.jsx
@@ -1,27 +1,55 @@
 import { useContext, useEffect, useState } from "react";
-import { getFestivals } from "/src/api_utils/festivalUtil"
-import {configContext} from '/src/App';
-function MainSec4 (){
-    const config = useContext(configContext);
-    const [festivals,setFestivals]= useState([])
-    useEffect(()=>{
-        getFestivals({
-            itemsPerPage:10,
-            pageNum:1,
-            language:config.language,
-            periodType:1,
-            sortMethod:1, 
-        },(response)=>{
-            console.log(response);
-            setFestivals(response.data);
-        },(error)=>{
-            console.log(error);
-        });     
+import { getFestivals } from "/src/api_utils/festivalUtil";
+import { configContext } from "/src/App";
+import React from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import {FestivalCard} from '/src/components/generic/festival/FestivalCard'
+import 'swiper/css';
+import 'swiper/css/pagination';
+import "./sec4.css";
+import { Autoplay} from 'swiper/modules';
+function MainSec4() {
+  const config = useContext(configContext);
+  const [festivals, setFestivals] = useState([]);
+  useEffect(() => {
+    getFestivals(
+      {
+        itemsPerPage: 10,
+        pageNum: 1,
+        language: config.language,
+        periodType: 1,
+        sortMethod: 1,
+      },
+      (response) => {
+        console.log(response);
+        setFestivals(response.data);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }, []);
+  console.log(festivals);
 
-    },[]) 
-    console.log(festivals)
-    return<>
-    
+  return (
+    <>
+     <Swiper
+        loop={true}
+        slidesPerView={3}
+        spaceBetween={30}
+        autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay]}
+        className="mySwiper"
+      >
+        {festivals.map((festival)=>(
+        <SwiperSlide key={festival.festival_id}> 
+        <FestivalCard festival={festival}></FestivalCard></SwiperSlide>
+      ))}
+      </Swiper>
     </>
+  );
 }
-export default MainSec4
+export default MainSec4;
