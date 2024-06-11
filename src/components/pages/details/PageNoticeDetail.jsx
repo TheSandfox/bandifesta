b49bf6324a4495a6cb0301ec0d3967165a4dea85
pcm5433 <pcm5433@naver.com>
2024-06-11 13:02:51 +0900

    0611

--- src/components/pages/details/PageNoticeDetail.jsx
+++ src/components/pages/details/PageNoticeDetail.jsx
@@ -1,3 +1,49 @@
-export default function PageNoticeDetail({}) {
-	return <></>
+import {useContext} from 'react';
+import {Routes, Route, Link, useParams, useNavigate} from 'react-router-dom';
+import {editContext, dataContext} from '../../../App';
+import './PageNoticeDetail.css';
+
+function PageNoticeDetail(){
+
+    const datas = useContext(dataContext);
+
+    let params = useParams();
+
+    let data = params.id;
+
+    const navigate = useNavigate();
+
+    const {removeNotice} = useContext(editContext);
+
+    function removeBtn(){
+        if(window.confirm("해당 게시글을 삭제하시겠습니까?")){
+            removeNotice(datas[data-1].id)
+            navigate("/notice")
+        }
+    }
+
+    return(
+        <>
+            <section key={datas[data-1].id} className='innerbox mainContent'>
+                <article className='noticeDetail'>
+                    <h2 className='fontTitle'>{datas[data-1].title}</h2>
+                    <div>
+                        <div className='fontMain detailTit'>작성자</div>
+                        <div className='fontMain detailBox'>{datas[data-1].name}</div>
+                    </div>
+                    <div>
+                        <div className='fontMain detailTit'>작성일</div>
+                        <div className='fontMain detailBox'>{datas[data-1].createDate}</div>
+                    </div>
+                    <p className='fontMain'>{datas[data-1].content}</p>
+                </article>
+                <article className='noticeBtn'>
+                    <button><Link to={`/notice/edit/${datas[data-1].id}`}>수정</Link></button>
+                    <button onClick={removeBtn}>삭제</button>
+                </article>
+            </section>
+        </>
+    )
 }
+
+export default PageNoticeDetail;
