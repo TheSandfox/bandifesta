8b727f6ebf3d7e5614e6dfacf07c6fd85fcaf45c
thesandfox <sandfox9411@gmail.com>
2024-06-11 15:42:59 +0900

    조아요버튼작동확인

--- src/api_utils/festivalUtil.js
+++ src/api_utils/festivalUtil.js
@@ -82,9 +82,81 @@ const getFestivalDetail = async(params,thenCallback,catchCallback,finallyCallbac
 	});
 }
 
+//축제 좋아요를 실행합니다.
+/*{
+	festivalId: 축제ID,
+	userId: 유저ID,
+	flag: 추가/삭제
+}*/
+const likeFestival = async(params,thenCallback,catchCallback,finallyCallback)=>{
+	await axios.get(import.meta.env.VITE_REST_URL+'/festival/likeFestival',{
+		params
+	})
+	.then(function (response) {
+		// 성공 핸들링
+		thenCallback(response);
+	})
+	.catch(function (error) {
+		// 에러 핸들링
+		if(catchCallback){catchCallback(error);}
+	})
+	.finally(function () {
+		// 항상 실행되는 영역
+		if(finallyCallback){finallyCallback();}
+	});
+}
+
+//축제의 좋아요 여부를 확인합니다
+/*{
+	festivalId: 축제ID,
+	userId: 유저ID
+}*/
+const isFestivalLiked = async(params,thenCallback,catchCallback,finallyCallback)=>{
+	await axios.get(import.meta.env.VITE_REST_URL+'/festival/isFestivalLiked',{
+		params
+	})
+	.then(function (response) {
+		// 성공 핸들링
+		thenCallback(response);
+	})
+	.catch(function (error) {
+		// 에러 핸들링
+		if(catchCallback){catchCallback(error);}
+	})
+	.finally(function () {
+		// 항상 실행되는 영역
+		if(finallyCallback){finallyCallback();}
+	});
+}
+
+//축제의 좋아요 갯수를 가져옵니다
+/*{
+	festivalId: 축제ID,
+}*/
+const getFestivalLikeCount = async(params,thenCallback,catchCallback,finallyCallback)=>{
+	await axios.get(import.meta.env.VITE_REST_URL+'/festival/getFestivalLikeCount',{
+		params
+	})
+	.then(function (response) {
+		// 성공 핸들링
+		thenCallback(response);
+	})
+	.catch(function (error) {
+		// 에러 핸들링
+		if(catchCallback){catchCallback(error);}
+	})
+	.finally(function () {
+		// 항상 실행되는 영역
+		if(finallyCallback){finallyCallback();}
+	});
+}
+
 export {
 	getFestivalPeriodTypes,
 	getFestivalSortMethods,
 	getFestivals,
-	getFestivalDetail
+	getFestivalDetail,
+	likeFestival,
+	isFestivalLiked,
+	getFestivalLikeCount
 }
--- src/components/generic/festival/FestivalCard.jsx
+++ src/components/generic/festival/FestivalCard.jsx
@@ -2,15 +2,53 @@ import { Link } from 'react-router-dom';
 import './festival.css';
 import { useEffect, useRef, useState } from 'react';
 import GenericTag from '../GenericTag';
+import { getKakaoUser } from '/src/api_utils/loginUtil';
+import { isFestivalLiked } from '/src/api_utils/festivalUtil';
+import { likeFestival } from '/src/api_utils/festivalUtil';
 
-function FestivalLikeButton({festivalId,userId}) {
+function FestivalLikeButton({festivalId}) {
 	const [pressed,setPressed] = useState(false);
-	const handlePressed = {
-		toggle:()=>{
-			setPressed(!pressed);
-		}
+	//최초 마운트시 좋아요여부 확인
+	useEffect(()=>{
+		getKakaoUser({
+
+		},(response)=>{
+			//유저정보받기 성공
+			isFestivalLiked({
+				userId:response.data['id'],
+				festivalId:festivalId
+			},(response2)=>{
+				// console.log(Boolean(response2.data))
+				setPressed(Boolean(response2.data));
+			},(error2)=>{
+
+			})
+		},(error)=>{
+			console.log(error);
+		})
+	},[])
+	//좋아요버튼 콜백
+	const likeRequest = ()=>{
+		getKakaoUser({
+			
+		},(response)=>{
+			//유저정보받기 성공
+			console.log(festivalId);
+			likeFestival({
+				userId:response.data['id'],
+				festivalId:festivalId,
+				flag:String(!pressed)
+			},(response2)=>{
+				//좋아요 반영
+				setPressed(!pressed);
+			},(error2)=>{
+
+			})
+		},(error)=>{
+			console.log(error);
+		});
 	}
-	return <div className='festivalLikeButton' onClick={handlePressed.toggle}>
+	return <div className='festivalLikeButton' onClick={likeRequest}>
 		<img className={'heart'} src={`/bandifesta/assets/${pressed?'heartFill':'heart'}.png`} alt={'축제 좋아요 버튼'}/>
 	</div>
 }
@@ -49,7 +87,7 @@ function FestivalCard({festival,disableTag}) {
 				alt={festival.title} 
 				className='festivalCardImage'
 				ref={imgElement}/>
-			<FestivalLikeButton/>
+			<FestivalLikeButton festivalId={festival.festival_id}/>
 		</div>
 		{/* 진,예,마 태그 */}
 		{
