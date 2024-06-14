0f5862b969a693f88838be58ecece11b5b9746a1
pcm5433 <pcm5433@naver.com>
2024-06-14 14:30:27 +0900

    0614

--- src/components/pages/notice/SubNoticeList.jsx
+++ src/components/pages/notice/SubNoticeList.jsx
@@ -1,18 +1,26 @@
+import {useState} from 'react';
 import {Routes, Route, Link} from 'react-router-dom';
 
 function noticeList(data){
+
+        const [count, setCount] = useState(0);
+
+        const onIncrease = ()=>{
+            setCount(prevCount => prevCount +1);
+        }
+
     return(
         <ul key={data.id} className='noticeListM'>
             <li className='notiNumber'>{data.id}</li>
             <li className='notiTitle'>
-                <Link to={`/notice/detail/${data.id}`}>
+                <Link to={`/notice/detail/${data.id}`} onClick={onIncrease}>
                     <span className='notiTitTxt'>{data.title}</span>
                     <img className='notiArrow' src="/bandifesta/assets/arrowGrey.png" alt="" />
                 </Link>
             </li>
             <li className='notiWriter'>{data.name}</li>
             <li className='notiDate'>{data.createDate}</li>
-            <li className='notiView'>400</li>
+            <li className='notiView'>{count}</li>
         </ul>
     )
 }
