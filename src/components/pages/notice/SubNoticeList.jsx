2d59c41b6905eb6692db966c51133b7de2d2e316
pcm5433 <pcm5433@naver.com>
2024-06-17 02:00:56 +0900

    0617

--- src/components/pages/notice/SubNoticeList.jsx
+++ src/components/pages/notice/SubNoticeList.jsx
@@ -6,14 +6,15 @@ function noticeList(data){
         const [count, setCount] = useState(0);
 
         const onIncrease = ()=>{
-            setCount(prevCount => prevCount +1);
+            setCount(count+1);
         }
+
 
     return(
         <ul key={data.id} className='noticeListM'>
             <li className='notiNumber'>{data.id}</li>
             <li className='notiTitle'>
-                <Link to={`/notice/detail/${data.id}`} onClick={onIncrease}>
+                <Link to={`/notice/detail/${data.id}`} onClick={onIncrease} onIncrease={onIncrease}>
                     <span className='notiTitTxt'>{data.title}</span>
                     <img className='notiArrow' src="/bandifesta/assets/arrowGrey.png" alt="" />
                 </Link>
